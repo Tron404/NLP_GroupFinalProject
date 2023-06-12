74dda30ae1f20522a948f73493075d549364efc0
Tron404 <edi.raul@yahoo.com>
2023-06-12 02:38:47 +0200

    fix

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from dataloader import *
 from architecture import *
 from training import *
 
-BUFFER_SIZE = 32000
-BATCH_SIZE = 64
-# Let's limit the #training examples for faster training
-num_examples = 30000
+BUFFER_SIZE = 50000
+BATCH_SIZE = 32
+num_examples = 45000
 
 tf.keras.backend.clear_session()
 
@@ -20,7 +19,7 @@ vocab_tar_size = len(targ_lang.word_index)+1
 max_length_input = example_input_batch.shape[1]
 max_length_output = example_target_batch.shape[1]
 
-embedding_dim = 256
+embedding_dim = 512
 units = 1024
 steps_per_epoch = num_examples//BATCH_SIZE
 
@@ -31,6 +30,6 @@ decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE, max_length_i
 
 lstm_model = LSTM_custom(encoder, decoder, units, max_length_input, dataset_creator, BATCH_SIZE)
 
-lstm_model.train(train_dataset, val_dataset, 10, steps_per_epoch, patience=2)
+lstm_model.train(train_dataset, val_dataset, 150, steps_per_epoch, patience=10)
 
 lstm_model.translate(inp_lang, targ_lang, u"Buna!")
