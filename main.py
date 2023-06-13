0461758bd6e17a7acd3fcf66ac0b3de3a7bcb1be
unknown <seba270270270@gmail.com>
2023-06-13 13:25:09 +0200

    reset back the older parameters

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from architecture import *
 from training import *
 from evaluation_metrics import *
 
-BUFFER_SIZE = 6200
+BUFFER_SIZE = 62000
 BATCH_SIZE = 100
-num_examples = 6000
+num_examples = 60000
 
 tf.keras.backend.clear_session()
 
@@ -20,8 +20,8 @@ vocab_tar_size = len(targ_lang.word_index)+1
 max_length_input = example_input_batch.shape[1]
 max_length_output = example_target_batch.shape[1]
 
-embedding_dim = 76
-units = 51
+embedding_dim = 768
+units = 512
 steps_per_epoch = num_examples//BATCH_SIZE
 
 print(max_length_input)
@@ -31,7 +31,7 @@ decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE, max_length_i
 
 lstm_model = LSTM_custom(encoder, decoder, units, max_length_input, dataset_creator, BATCH_SIZE)
 
-lstm_model.train(train_dataset, val_dataset, 1, steps_per_epoch, patience=5)
+lstm_model.train(train_dataset, val_dataset, 150, steps_per_epoch, patience=5)
 
 # lstm_model.translate(inp_lang, targ_lang, u"Buna!")
 
